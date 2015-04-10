df4a19be033680e64dac0af99e93e014f113dde3
davidyack <dyack@coloradotc.com>
2015-04-09 19:16:27 -0600

    changed to devdependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -214,7 +214,9 @@ module.exports = function (grunt) {
       target: {
         src: '<%= yeoman.client %>/index.html',
         ignorePath: '<%= yeoman.client %>/',
-        exclude: [/jquery/, /bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/ ]
+        exclude: [/jquery/, /bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/ ],
+        devDependencies: true
+  
       }
     },
 
